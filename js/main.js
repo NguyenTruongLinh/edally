84ce171e39f9f3a2233fb6a7fc48c3079efd8196
linh <nhokkuteo1996@gmail.com>
2020-04-04 22:07:04 +0700

    Update code

--- js/main.js
+++ js/main.js
@@ -1,10 +1,40 @@
 $(document).ready(function() {
-    $('.owl-carousel').owlCarousel({
+    $('#license').owlCarousel({
         loop: true,
         items: 1,
         margin: 0,
         nav: true,
         dots: false
+    });
+    $('#many-users').owlCarousel({
+        loop: true,
+        items: 2,
+        margin: 10,
+        nav: false,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        slideBy: 2
+    });
+    $('#owl-blog').owlCarousel({
+        loop: false,
+        items: 3,
+        margin: 0,
+        nav: false,
+        dots: false,
+        responsive: {
+            0: {
+                items: 2,
+                center: true,
+                loop: true,
+                margin: 30
+            },
+            768: {
+                items: 3,
+                margin: 30,
+                stagePadding: 10
+            }
+        }
     });
 
     let minNumber = 1;
@@ -19,4 +49,42 @@ $(document).ready(function() {
             $('.quantity input').val(parseInt($('.quantity input').val(), 10) - 1);
         }
     });
+
+    $('.close-menu-mb').click(function() {
+        if ($('.navbar-collapse').hasClass('show')) {
+            $('.navbar-collapse').removeClass('show');
+            $('body').removeClass('overflow-hidden');
+        }
+    });
+
+    $('.nav-mobile').click(function() {
+        $('.navbar-nav, .navbar-collapse').addClass('show');
+        $('body').addClass('overflow-hidden');
+    });
+
+    if ($(window).width() < 767) {
+        $('.wow').removeClass('wow')
+    }
+
+    new WOW().init();
 });
+
+(function fixedTopMenu(fixedMenuSelector, fixedMenuDisplayClass, anchorSelector) {
+    var $b = $(document.body),
+        $d = $(document);
+
+    function fixTopMenuAction() {
+        var $topMenu = $(fixedMenuSelector);
+        if (Boolean($(anchorSelector).eq(0).offset())) {
+            var currentScroll = $(this).scrollTop();
+            if (currentScroll >= $(anchorSelector).eq(0).offset().top) {
+                $topMenu.addClass(fixedMenuDisplayClass);
+            } else {
+                $topMenu.removeClass(fixedMenuDisplayClass);
+            }
+        }
+    }
+    fixTopMenuAction();
+
+    $(window).on('scroll load', fixTopMenuAction);
+})('.stickyTrigger', 'is-stick', '.sticky-here');
